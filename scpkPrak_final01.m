bc7580193ca90f5223d320d8375ce92e9bd85281
Rowang P <54165958+RowPramudito@users.noreply.github.com>
2022-05-26 13:42:01 +0700

    Update scpkPrak_final01.m

--- scpkPrak_final01.m
+++ scpkPrak_final01.m
@@ -1,5 +1,3 @@
-
-
 dataset = xlsread('ahp_alts.xlsx','Sheet1','B2:D22');
 
 c1 = zeros(21,1);
@@ -7,45 +5,35 @@ c2 = zeros(21,1);
 c3 = zeros(21,1);
 
 for i=1:21,
-    c1(i,1) = dataset(i,1);
-    c2(i,2) = dataset(i,2);
-    c3(i,3) = dataset(i,3);
+    c1(i,1) = dataset(1,1) / dataset(i,1);
+    c2(i,1) = dataset(1,2) / dataset(i,2);
+    c3(i,1) = dataset(1,3) / dataset(i,3);
 end;
 
-c1b = calc_norm(c1);
-[m n] = size(c1b);
+c1w = calc_norm(c1);
+
+c2w = calc_norm(c2);
+
+c3w = calc_norm(c3);
+
+
+MPB = [1 1 1;
+       1 1 1;
+       1 1 1];
+MPBw = calc_norm(MPB);
+[m n] = size(MPBw);
 for i=1:m,
     sumRow = 0;
     for j=1:n,
-        sumRow = sumRow + c1b(i,j);
+        sumRow = sumRow + MPBw(i,j);
     end;
     V(i) = (sumRow);
 end;
-c1b = transpose(V) / m;
+MPBw = transpose(V)/m;
 
-c2b = calc_norm(c2);
-[m n] = size(c2b);
-for i=1:m,
-    sumRow = 0;
-    for j=1:n,
-        sumRow = sumRow + c2b(i,j);
-    end;
-    V(i) = (sumRow);
-end;
-c2b = transpose(V) / m;
-
-c3b = calc_norm(c3);
-[m n] = size(c3b);
-for i=1:m,
-    sumRow = 0;
-    for j=1:n,
-        sumRow = sumRow + c3b(i,j);
-    end;
-    V(i) = (sumRow);
-end;
-c3b = transpose(V) / m;
-
-
+wM = [c1w c2w c3w];
+MC_scores = wM * MPBw;
+result = max(MC_scores);
 
 function [normvect ] = calc_norm(M)
     sM = sum(M);
